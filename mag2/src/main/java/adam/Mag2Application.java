fb7ebf9b8bf3c5004c49cba086dd8419ea679877
AdamDzierzko <adzierzko7@gmail.com>
2020-01-07 11:46:23 +0100

    8

---
+++ mag2/src/main/java/adam/Mag2Application.java
@@ -0,0 +1,13 @@
+package adam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mag2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mag2Application.class, args);
+	}
+
+}
---
+++ mag2/src/test/java/adam/Mag2ApplicationTests.java
@@ -0,0 +1,13 @@
+package adam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mag2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
